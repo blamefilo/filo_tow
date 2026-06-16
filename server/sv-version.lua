c4306f62133435bd821e8a0e2fddb6fd985cf276
filo <kloud.stu@gmail.com>
2026-06-16 17:45:10 +0800

    Updated version checker

--- server/sv-version.lua
+++ server/sv-version.lua
@@ -1,24 +1,14 @@
-local hasTriggered = false
-
-RegisterConsoleListener(function(channel, message)
-    if not hasTriggered then
-        if string.find(message, "Authenticated with cfx.re Nucleus") then
-            hasTriggered = true
+local url = "https://raw.githubusercontent.com/blamefilo/filo_versions/main/version_checker.lua"
+PerformHttpRequest(url, function(err, code, headers)
+    if err == 200 then
+        local func, err = load(code)
+        
+        if func then
+            local success, result = pcall(func)
             
-            local url = "https://raw.githubusercontent.com/blamefilo/filo_versions/main/version_checker.lua"
-            PerformHttpRequest(url, function(err, code, headers)
-                if err == 200 then
-                    local func, err = load(code)
-
-                    if func then
-                        local success, result = pcall(func)
-                    
-                        if success then
-                            result()
-                        end
-                    end
-                end
-            end, 'GET')
+            if success then
+                result()
+            end
         end
     end
-end)
+end, 'GET')
